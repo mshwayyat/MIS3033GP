6754bd4a46449c15837c9e1425aa5a76a26e258a
Lauren LeMasters <laurenlemasters@ou.edu>
2022-04-27 20:00:50 -0500

    Conencted to database, made GUI, and added a second window for grade.

---
+++ Loves_Project/Loves_WPF/Grade.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Loves_WPF
+{
+    /// <summary>
+    /// Interaction logic for Grade.xaml
+    /// </summary>
+    public partial class Grade : Window
+    {
+        public Grade()
+        {
+            InitializeComponent();
+        }
+    }
+}
